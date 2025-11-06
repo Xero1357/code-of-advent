8cbf5cde99e25652f6234535fb3be77d348ccc2d
Xero1357 <emir90@live.nl>
2025-11-06 13:26:35 +0100

    Delete 2022 directory

--- 2022/day 3/day3.py
+++
@@ -1,5 +0,0 @@
-with open("C:\\Users\\EC\\Downloads\\the maps\\vinegar\\knowledge-skill and trade\\adventofcode challenge\\2022\\day 3\\data.txt", "r") as file:
-    map = file.read()
-    A = map.split('\n') 
-print(A)
-
